fc44ae324a0cf2a4e7cec4e04e04756219dea247
colinweber <colinweber@4ff52acd-b852-0410-a751-8150d1dffee0>
2008-08-07 20:39:03 +0000

    Added Identity and Name to X-Model content importer.

--- Model/XModelProcessor.cs
+++ Model/XModelProcessor.cs
@@ -25,22 +25,54 @@ namespace ContentModel
                                                 ContentProcessorContext context)
         {
             //System.Diagnostics.Debugger.Launch();
-            BasicMaterialContent basicMaterial = material as BasicMaterialContent;
+            //the new effectmaterial we will be returning
+            EffectMaterialContent effectMaterial;
 
-            EffectMaterialContent effectMaterial = new EffectMaterialContent();
-
-            // Store a reference to our skinned mesh effect.
-            string effectPath = "../../X-Engine/Content/XEngine/Effects/Model.fx";
-
-            effectMaterial.Effect = new ExternalReference<EffectContent>(effectPath);
-
-            // Copy texture settings from the input
-            // BasicMaterialContent over to our new material.
-            if (basicMaterial.Texture != null)
+            if (material is BasicMaterialContent)
             {
-                effectMaterial.Textures.Add("Texture", basicMaterial.Texture);
-            }
+                
+                BasicMaterialContent basicMaterial = material as BasicMaterialContent;
 
+                effectMaterial = new EffectMaterialContent();
+                // Store a reference to our skinned mesh effect.
+                effectMaterial.Effect = new ExternalReference<EffectContent>("../../X-Engine/Content/XEngine/Effects/Model.fx");
+                
+                //copy over TextureReferenceDictionary
+                //this won't work tex.name is allways null!
+                //foreach(ExternalReference<TextureContent> tex in basicMaterial.Textures.Values)
+                //{
+                //    effectMaterial.Textures.Add(tex.Name, tex);
+                //}
+
+                // Copy texture settings from the input
+                // BasicMaterialContent over to our new material.
+                // Supposed to support multiple textures but I just take 1 (diffuse texture)
+                if (basicMaterial.Texture != null)
+                    effectMaterial.Textures.Add("Texture", basicMaterial.Texture);
+
+                if (basicMaterial.Identity != null)
+                    effectMaterial.Identity = basicMaterial.Identity;
+
+                if (basicMaterial.Name != null)
+                    effectMaterial.Name = basicMaterial.Name;
+
+                //foreach(object obj in basicMaterial.OpaqueData)
+                //        effectMaterial.OpaqueData.Add(obj.ToString
+
+                // And you can also set any arbitrary effect parameters at this point
+                //myMaterial.OpaqueData.Add("Shininess", basicMaterial.SpecularPower * 10);
+                //myMaterial.OpaqueData.Add("BumpSize", 42);
+            }
+            else if (material is EffectMaterialContent)
+            {
+                effectMaterial =  (EffectMaterialContent)material;
+                // todo: put something interesting here if you want
+                // or don't bother if you don't want
+                // whatever really...
+            }
+            else
+                throw new Exception("huh? this is very odd");
+            
             // Chain to the base ModelProcessor converter.
             return base.ConvertMaterial(effectMaterial, context);
         }
