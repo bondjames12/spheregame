97fd797879fbf9e7759773ccb7a552f8b273853d
colinweber <colinweber@4ff52acd-b852-0410-a751-8150d1dffee0>
2008-08-07 21:37:38 +0000

    Add XSI XNA Runtime. Supports model animations!

---
+++ trunk/XSIXNARuntime/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XSIXNARuntime")]
+[assembly: AssemblyProduct("XSIXNARuntime")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Avid")]
+[assembly: AssemblyCopyright("Copyright © Avid 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be0e6dc1-551f-44f5-97e9-b216ad956e93")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0.0.0")]
